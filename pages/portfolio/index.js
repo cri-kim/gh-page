9b8a0977b30f1a5e87333ec89f343171acb34d4b
DESKTOP-4TTG0LD\kcri <67crystalk@naver.com>
2021-09-15 23:24:04 +0900

    # Create : 고양이, 움직이는 구름 컴포넌트 작성 ## 태그 - css - 컴포넌트 - 애니메이션

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,5 +1,5 @@
 import Layout from '@/components/portfolio/layout'
-import Temp1 from '@/components/portfolio/temp1'
+import Cloud from '@/components/avatars/cloud'
 import Cat from '@/components/avatars/cat'
 import React, {Component} from 'react';
 
@@ -17,17 +17,16 @@ class Home extends Component {
     return (
       <Layout>
           <div className="p-5 mb-4 rounded-3">
-            <Temp1 >{this.state.children}</Temp1>
+            <Cloud/>
             <div className="col-md-8 p-lg-8 mx-auto my-5">
-              <h1 className="display-4 fw-normal"><i>김수정</i> 입니다.</h1>
-              <p className="lead fw-normal">원활한 의사소통, 발전을 위한 학습, 그리고 끈기를 갖고 오늘도 모니터와 함께있습니다.</p>
+              <h1 className="display-4 fw-normal">Welcome.(❁´◡`❁)</h1>
             </div>
           </div>
           <div className="row align-items-md-stretch">
             <div className="col-md-6">
             
             </div>
-            <div className="col-md-6 position-relative">
+            <div className="col-md-6">
               <Cat onCreate={this.handleCreate}></Cat>
             </div>
           </div>
